12620ea44ac814379376decfee5ac0fec250fcaf
daniellebortoleto <danielle.bortoleto3412@gmail.com>
2025-05-01 18:12:55 -0300

    Initial commit.

---
+++ app/Filament/Resources/ItemResource.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\ItemResource\Pages;
+use App\Filament\Resources\ItemResource\RelationManagers;
+use App\Models\Item;
+use Filament\Forms;
+use Filament\Forms\Components\MarkdownEditor;
+use Filament\Forms\Components\Radio;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Columns\SelectColumn;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class ItemResource extends Resource
+{
+    protected static ?string $model = Item::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('titulo')
+                    ->label('Titulo')
+                    ->required(),
+                Radio::make('tipo')
+                    ->label('Tipo de Objetivo')
+                    ->options([
+                        'max' => 'Maximização',
+                        'min' => 'Minimização',
+                    ])
+                        ->required()
+                        ->inline(),
+                MarkdownEditor::make('descricao')
+                    ->label('Descricao')
+                    ->columnSpanFull(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('titulo')
+                    ->label('Titulo'),
+                TextColumn::make('descricao')
+                    ->label('Descricao'),
+                TextColumn::make('tipo')
+                    ->label('Tipo de Objetivo')
+                    ->formatStateUsing(fn (string $state) => $state === 'max' ? 'Maximização' : 'Minimização'),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListItems::route('/'),
+            'create' => Pages\CreateItem::route('/create'),
+            'edit' => Pages\EditItem::route('/{record}/edit'),
+        ];
+    }
+}
